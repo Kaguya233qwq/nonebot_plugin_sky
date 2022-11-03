355a050786757a64e525227cc1e01fe020bdf1ee
neet姬辉夜 <1435608435@qq.com>
2022-11-03 15:00:53 +0800

    初次推送

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
+<div align="center">
+
+<p align="center">
+  <a href=""><img src="https://img1.baidu.com/it/u=3563761161,679242917&fm=253&fmt=auto&app=138&f=PNG?w=360&h=360" width="200" height="200" alt="kfc"></a>
+</p>
+
 # nonebot_plugin_sky
+
 nonebot2的光遇每日任务及活动查询插件
+
+</div>
+
+# 开始使用
+
+克隆本项目到本地，在bot.py中导入插件：
+
+`nonebot.load_plugin(r"包路径\nonebot_plugin_sky")`
+
+
+# 使用命令
+
+在有bot的群内发送`/光遇每日攻略`来获取今日的攻略
+数据来自微博@今天游离翻车了吗
+
+## TODO？
+
+* [ ]  每日干饭提醒小助手
+* [ ]  更多光遇攻略
+
+# 效果展示
+
+![](.README_images/test.jpg)
+
+## **联系我**
+
+<p align="center">
+  <a href="https://github.com/"><img src="https://github.com/Kaguya233qwq/nonebot_plugin_alicdk_get/blob/main/.README_images/17623ac4.png?raw=true" width="300" height="350" alt="QRCode"></a>
+</p>
+
+## 更新日志
+
+2022.11.3 v1.0
+
+初推版本
---
+++ nonebot_plugin_sky/__init__.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+# @Author  : 子龙君
+# @Email   :  1435608435@qq.com
+# @Github    : Kaguya
+# @Software: PyCharm
+
+import httpx
+import datetime
+import json
+
+import nonebot
+from nonebot import on_command, logger
+from nonebot.adapters.onebot.v11 import MessageSegment, Bot
+from nonebot.adapters.onebot.v11 import NetworkError as networkError
+
+daily_yoli = on_command("daily_yoli", aliases={"光遇今日攻略"})
+
+try:
+    BotName = nonebot.get_driver().config.nickname
+except Exception as e:
+    logger.error(e)
+    logger.warning('您还没有配置bot的昵称，请先进入env文件配置')
+
+
+class SkyDaily:
+    """光遇类"""
+
+    def __init__(self):
+        self.url = 'https://weibo.com/ajax/statuses/mymblog?uid=7360748659&page=1&feature=0'
+        self.longtext = 'https://weibo.com/ajax/statuses/longtext?id='
+        self.headers = {
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) '
+                          'AppleWebKit/537.36 (KHTML, like Gecko) Chrome'
+                          '/62.0.3202.9 Safari/537.36',
+            'cookie': 'SUB=_2AkMUd3SHf8NxqwFRmP8Ty2Pna4VwywzEieKiK4VcJRMxHRl'
+                      '-yT9jqnAOtRB6P_daaLXfdvYkPfvZhXy3bTeuLdBjWXF9;'
+        }
+        self.copyright_ = ('------------'
+                           '\r【数据来源：微博@今天游离翻车了吗】\n'
+                           '--本插件仅做数据展示之用，著作权归原文作者所有。'
+                           '转载或转发请附文章作者微博--')
+
+    @staticmethod
+    def get_today():
+        """获取今日日期"""
+        date = datetime.date.today().timetuple()
+        today = str(date.tm_mon) + '月' + str(date.tm_mday) + '日'
+        today_format = str(date.tm_mon) + '.' + str(date.tm_mday)
+        print('今天是：{}'.format(today))
+        return today_format
+
+    async def get_mblog_id(self):
+        """获取微博 @今天游离翻车了吗 顶置文章详情"""
+
+        async with httpx.AsyncClient() as client:
+            response = await client.get(
+                url=self.url,
+                headers=self.headers)
+            content = json.loads(response.text)
+            overhead = content['data']['list'][0]
+            return overhead
+
+    async def get_longtext(self, mblog_id: str):
+        """获取微博 @今天游离翻车了吗 顶置文章长文本"""
+
+        async with httpx.AsyncClient() as client:
+            response = await client.get(
+                url=self.longtext + mblog_id,
+                headers=self.headers)
+            content = json.loads(response.text)
+            longtext = content['data']['longTextContent']
+            return longtext
+
+    async def get_data(self):
+        """获取今日攻略数据"""
+        results = MessageSegment.text('')
+        today = self.get_today()
+        overhead = await self.get_mblog_id()
+        mblog_id = overhead['mblogid']
+        longtext = await self.get_longtext(mblog_id)
+        if today in longtext:
+            results += MessageSegment.text(longtext)
+            pic_infos = overhead['pic_infos']
+            for pic in pic_infos:
+                large_url = pic_infos[pic]['large']['url']
+                img = MessageSegment.image(large_url)
+                results += img
+            results += self.copyright_  # 附加版权信息
+        else:
+            notice = '今日数据还未更新，请耐心等候'
+            logger.warning('今日数据还未更新，请耐心等候')
+            results += MessageSegment.text(notice)
+        return results
+
+
+async def chain_reply(
+        # 构造聊天记录转发消息，参照了塔罗牌插件
+        bot: Bot,
+        msg: MessageSegment
+):
+    chain = []
+
+    data = {
+        "type": "node",
+        "data": {
+            "name": BotName,
+            "uin": f"{bot.self_id}",
+            "content": msg
+        },
+    }
+    chain.append(data)
+    return chain
+
+
+@daily_yoli.handle()
+async def _():
+    try:
+        sky = SkyDaily()
+        results = await sky.get_data()
+        await daily_yoli.send(results)
+
+    except networkError:
+        logger.error('NetworkError: 网络环境较差，调用发送信息接口超时')
+        await daily_yoli.send(
+            message='网络环境较差，调用发送信息接口超时'
+        )
