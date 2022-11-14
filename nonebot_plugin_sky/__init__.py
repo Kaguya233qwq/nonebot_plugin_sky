319fef979b14ff83ff0f21855b47a06eecbc6dad
zilong <1435608435@qq.com>
2022-11-14 15:51:45 +0800

    修复当游离未将今日攻略顶置时获取不到数据的问题

--- nonebot_plugin_sky/__init__.py
+++ nonebot_plugin_sky/__init__.py
@@ -46,7 +46,7 @@ class SkyDaily:
         date = datetime.date.today().timetuple()
         today = str(date.tm_mon) + '月' + str(date.tm_mday) + '日'
         today_format = str(date.tm_mon) + '.' + str(date.tm_mday)
-        print('今天是：{}'.format(today))
+        logger.info('今天是：{}'.format(today))
         return today_format
 
     async def get_mblog_id(self, today):
