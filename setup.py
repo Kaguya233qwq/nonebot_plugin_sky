cab5a9fb3ef44d521aebda60066efeeb871219c5
zilong <1435608435@qq.com>
2022-11-18 16:12:04 +0800

    新增获取官方公告命令

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8", errors="ignore") as f:
     long_description = f.read()
 setuptools.setup(
     name='nonebot-plugin-sky',
-    version='1.1.0',
+    version='1.1.1',
     author='Kaguya233qwq',
     author_email='1435608435@qq.com',
     keywords=["pip", "nonebot2", "nonebot", "sky光遇", "光遇"],
