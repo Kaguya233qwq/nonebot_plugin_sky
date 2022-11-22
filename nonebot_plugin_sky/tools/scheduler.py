b3f145ee837bfbb7f39f47526538354bd60e6b6e
neet姬辉夜 <1435608435@qq.com>
2022-11-22 17:31:37 +0800

    修复发送问题

--- nonebot_plugin_sky/tools/scheduler.py
+++ nonebot_plugin_sky/tools/scheduler.py
@@ -33,6 +33,10 @@ async def scheduler_handler(matcher: Matcher, args: Message = CommandArg()):
             if scheduler.state:
                 scheduler.shutdown()
                 await matcher.send('已关闭雨林干饭小助手')
+
+        elif '测试' in plain_text:
+            results = await go()
+            await matcher.send("【功能测试】\n" + results)
         else:
             await matcher.send('指令关键字错误请重新输入')
     except Exception as e:
@@ -42,24 +46,26 @@ async def scheduler_handler(matcher: Matcher, args: Message = CommandArg()):
         # 设定提前五分钟提醒
         @scheduler.scheduled_job("cron", hour="7,9,11,15,16,17,19", minute="32,55", id="job_0")
         async def auto_run():
-            texts = [
-                '干饭人干饭魂,冲鸭！',
-                '还有五分钟就要开饭了哦~快叫上你的朋友一起去叭',
-                '让我看看是哪个小可爱干饭不带盆~,'
-                '上号！干饭人',
-                '干饭不积极思想有问题~今天我要吃三碗饭！',
-                '快到饭点了崽崽们，准备准备出发吧~',
-                '我是要励志把小陈吃破产的人！'
-            ]
-            text = random.sample(texts, 1)[0]
-            # 读取绝对路径
-            abspath_ = os.path.abspath(__file__).strip('scheduler.py')
-            path = abspath_ + 'image\\'
-            image_list = os.listdir(path)
-            file = random.sample(image_list, 1)[0]
-            await matcher.send(
-                text + MessageSegment.image('file:///' + path + file)
-            )
+            results = await go()
+            await matcher.send(results)
 
     else:
         pass
+
+async def go():
+    texts = [
+        '干饭人干饭魂,冲鸭！',
+        '还有五分钟就要开饭了哦~快叫上你的朋友一起去叭',
+        '让我看看是哪个小可爱干饭不带盆~,'
+        '上号！干饭人',
+        '干饭不积极思想有问题~今天我要吃三碗饭！',
+        '快到饭点了崽崽们，准备准备出发吧~',
+        '我是要励志把小陈吃破产的人！'
+    ]
+    text = random.sample(texts, 1)[0]
+    abspath_ = os.path.abspath(__file__).strip('scheduler.py')
+    path = abspath_ + 'image\\'
+    image_list = os.listdir(path)
+    file = random.sample(image_list, 1)[0]
+    results = text + MessageSegment.image('file:///' + path + file)
+    return results
