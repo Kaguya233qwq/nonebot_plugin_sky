3233917d9f714e9699f86acfe2a3ce653f900ed1
沉默的子龙君 <1435608435@qq.com>
2022-11-26 21:45:09 +0800

    修正雨林干饭小助手的命令文档

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open("README.md", "r", encoding="utf-8", errors="ignore") as f:
     long_description = f.read()
 setuptools.setup(
     name='nonebot-plugin-sky',
-    version='1.2.0',
+    version='1.2.1',
     author='Kaguya233qwq',
     author_email='1435608435@qq.com',
     keywords=["pip", "nonebot2", "nonebot", "sky光遇", "光遇"],
