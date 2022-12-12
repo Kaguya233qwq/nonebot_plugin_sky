c5ae455537196ed4c11f72dfb71eee580cdf91d2
沉默的子龙君 <1435608435@qq.com>
2022-12-12 13:05:13 +0800

    修改国服复刻先祖的数据源

--- nonebot_plugin_sky/tools/progenitor_return.py
+++ nonebot_plugin_sky/tools/progenitor_return.py
@@ -9,7 +9,7 @@ class Return:
     """国服复刻类类"""
 
     def __init__(self):
-        self.url = 'https://weibo.com/ajax/statuses/mymblog?uid=5539106873&feature=0'
+        self.url = 'https://weibo.com/ajax/statuses/mymblog?uid=6182255793&feature=0'
         self.longtext = 'https://weibo.com/ajax/statuses/longtext?id='
         self.headers = {
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) '
@@ -19,12 +19,12 @@ class Return:
                       '-yT9jqnAOtRB6P_daaLXfdvYkPfvZhXy3bTeuLdBjWXF9;'  # 未登录时的cookie直接写死
         }
         self.copyright_ = ('------------'
-                           '\r【数据来源：微博@光遇陈陈】\n'
+                           '\r【数据来源：微博@张张幼稚园】\n'
                            '--本插件仅做数据展示之用，著作权归原文作者所有。'
                            '转载或转发请附文章作者微博--')
 
     async def get_mblog(self, params, max_page):
-        """获取微博 @光遇陈陈 复刻先祖详情"""
+        """获取微博 @张张幼稚园 复刻先祖详情"""
 
         for page in range(1, max_page + 1):
             param = {
@@ -39,7 +39,7 @@ class Return:
                 content = json.loads(response.text)
                 overhead = content['data']['list']
                 for log in overhead:
-                    if '【复刻】' in log['text_raw'] and params in log['text_raw']:
+                    if '本周复刻' in log['text_raw'] and params in log['text_raw']:
                         return log
         return None
 
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open("README.md", "r", encoding="utf-8", errors="ignore") as f:
     long_description = f.read()
 setuptools.setup(
     name='nonebot-plugin-sky',
-    version='1.2.8',
+    version='1.2.9',
     author='Kaguya233qwq',
     author_email='1435608435@qq.com',
     keywords=["pip", "nonebot2", "nonebot", "sky光遇", "光遇"],
