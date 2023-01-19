19d74c9441db053ebc1e33211658838e64bec465
沉默的子龙君 <1435608435@qq.com>
2023-01-19 20:22:53 +0800

    1.更换国服复刻数据源为微博@木易不高兴了啊 2.修复国际服复刻的匹配问题

--- nonebot_plugin_sky/tools/in_travelling_spirit.py
+++ nonebot_plugin_sky/tools/in_travelling_spirit.py
@@ -1,7 +1,48 @@
+import json
+
+import httpx
 from nonebot import logger
 from nonebot.adapters.onebot.v11 import MessageSegment
 
-from .travelling_spirit import Travelling
+
+class Travelling:
+    """国际服复刻类"""
+
+    def __init__(self):
+        self.url = 'https://weibo.com/ajax/statuses/mymblog?uid=5539106873&feature=0'
+        self.longtext = 'https://weibo.com/ajax/statuses/longtext?id='
+        self.headers = {
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) '
+                          'AppleWebKit/537.36 (KHTML, like Gecko) Chrome'
+                          '/62.0.3202.9 Safari/537.36',
+            'cookie': 'SUB=_2AkMUd3SHf8NxqwFRmP8Ty2Pna4VwywzEieKiK4VcJRMxHRl'
+                      '-yT9jqnAOtRB6P_daaLXfdvYkPfvZhXy3bTeuLdBjWXF9;'  # 未登录时的cookie直接写死
+        }
+        self.copyright_ = ('------------'
+                           '\r【数据来源：微博@木易不高兴了啊】\n'
+                           '--本插件仅做数据展示之用，著作权归原文作者所有。'
+                           '转载或转发请附文章作者微博--')
+
+    async def get_mblog(self, params, max_page):
+        """获取微博 @光遇陈陈 国际服复刻先祖详情"""
+
+        for page in range(1, max_page + 1):
+            param = {
+                'page': str(page)
+            }
+            async with httpx.AsyncClient() as client:
+                response = await client.get(
+                    url=self.url,
+                    headers=self.headers,
+                    params=param
+                )
+                content = json.loads(response.text)
+                overhead = content['data']['list']
+                for log in overhead:
+                    if ('【复刻】' in log['text_raw']
+                            and params in log['text_raw']):
+                        return log
+        return None
 
 
 async def get_data():
--- nonebot_plugin_sky/tools/travelling_spirit.py
+++ nonebot_plugin_sky/tools/travelling_spirit.py
@@ -9,7 +9,7 @@ class Travelling:
     """国服复刻类"""
 
     def __init__(self):
-        self.url = 'https://weibo.com/ajax/statuses/mymblog?uid=5539106873&feature=0'
+        self.url = 'https://weibo.com/ajax/statuses/mymblog?uid=7444390196&feature=0'
         self.longtext = 'https://weibo.com/ajax/statuses/longtext?id='
         self.headers = {
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) '
@@ -19,12 +19,12 @@ class Travelling:
                       '-yT9jqnAOtRB6P_daaLXfdvYkPfvZhXy3bTeuLdBjWXF9;'  # 未登录时的cookie直接写死
         }
         self.copyright_ = ('------------'
-                           '\r【数据来源：微博@光遇陈陈】\n'
+                           '\r【数据来源：微博@木易不高兴了啊】\n'
                            '--本插件仅做数据展示之用，著作权归原文作者所有。'
                            '转载或转发请附文章作者微博--')
 
     async def get_mblog(self, params, max_page):
-        """获取微博 @光遇陈陈 复刻先祖详情"""
+        """获取微博 @木易不高兴了啊 复刻先祖详情"""
 
         for page in range(1, max_page + 1):
             param = {
@@ -40,7 +40,8 @@ class Travelling:
                 overhead = content['data']['list']
                 for log in overhead:
                     if ('本周复刻' in log['text_raw'] or
-                            '#光遇复刻#' in log['text_raw']
+                            '#光遇复刻#' in log['text_raw'] or
+                            '复刻' in log['text_raw']
                             and params in log['text_raw']):
                         return log
         return None
