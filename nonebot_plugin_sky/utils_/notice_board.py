d79a13dbd6033d6a52df654a759596c5514cf849
zilong <1435608435@qq.com>
2023-04-04 15:28:38 +0800

    修复插件公告镜像源地址

--- nonebot_plugin_sky/utils_/notice_board.py
+++ nonebot_plugin_sky/utils_/notice_board.py
@@ -2,13 +2,14 @@ import httpx
 from bs4 import BeautifulSoup
 import logging
 from nonebot import on_command
+from nonebot.adapters.onebot.v11 import MessageSegment
 from ..config.command import get_cmd_alias
 
 logging.captureWarnings(True)  # 去掉建议使用SSL验证的显示
 
 
 async def get_plugin_notice():
-    url = 'https://gitee.com/Kaguya233qwq/notice_manager/blob/main/public_sky/notice.txt'
+    url = 'https://gitee.com/Kaguyaaa/notice_manager/blob/main/public_sky/notice.txt'
     headers = {
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) '
                       'AppleWebKit/537.36 (KHTML, like Gecko) Chrome'
@@ -24,6 +25,7 @@ async def get_plugin_notice():
         notice_list = bs.find_all(class_='line')
         for notice in notice_list:
             results_ += notice.text
+        print(results_)
         return results_
 
 
@@ -33,7 +35,7 @@ Notice = on_command("noticeboard", aliases=get_cmd_alias('noticeboard'))
 @Notice.handle()
 async def notice_handle():
     results_ = await get_plugin_notice()
-    await Notice.send(results_)
+    await Notice.send(MessageSegment.text(results_))
 
 
 __all__ = (
