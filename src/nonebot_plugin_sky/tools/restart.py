32fe92c06c534c155cb014a1be8080639a43ceaf
kaguya <1435608435@qq.com>
2025-09-21 15:25:59 +0800

    ✨ feat: add signal file on restarted

--- src/nonebot_plugin_sky/tools/restart.py
+++ src/nonebot_plugin_sky/tools/restart.py
@@ -1,9 +1,15 @@
 import asyncio
+from enum import Enum
+import json
 import os
+from pathlib import Path
+import aiofiles
 from nonebot import logger
+from nonebot.matcher import Matcher
+from nonebot.adapters.onebot.v11 import Bot, MessageEvent, GroupMessageEvent
 
 
-async def restart_handle(matcher):
+async def restart_handle(matcher: Matcher, event: MessageEvent):
     """
     重启bot框架
     前提是需要通过sky脚手架命令运行框架
@@ -22,7 +28,6 @@ async def restart_handle(matcher):
         """
         logger.info("插件更新成功，正在调度重启...")
 
-        # 1. 获取当前正在运行的事件循环
         try:
             loop = asyncio.get_running_loop()
         except RuntimeError:
@@ -31,5 +36,111 @@ async def restart_handle(matcher):
             return
         loop.call_later(0, _do_exit, 0)
 
-    await matcher.send("正在重启插件...")
+    await matcher.send("正在重启Bot...")
+    tips = "Bot重启完成"
+    if isinstance(event, GroupMessageEvent):
+        action = Action.SEND_GROUP_MSG
+        signal = Signal(
+            name="Send message when connected",
+            action=action,
+            group_id=event.group_id,
+            message=tips,
+        )
+    else:
+        action = Action.SEND_PRIVATE_MSG
+        signal = Signal(
+            name="Send message when connected",
+            action=action,
+            user_id=event.user_id,
+            message=tips,
+        )
+    await register_signal(signal)
     await trigger_restart()
+
+
+SIGNAL_PATH = Path("signal.json")
+
+
+class Action(Enum):
+    SEND_GROUP_MSG = "send_group_msg"
+    SEND_PRIVATE_MSG = "send_private_msg"
+
+
+class Signal:
+    def __init__(
+        self,
+        name: str,
+        action: Action,
+        *,
+        user_id: int | None = None,
+        group_id: int | None = None,
+        message: str | None = None,
+    ):
+        self.name = name
+        self.action = action
+        self.user_id = user_id
+        self.group_id = group_id
+        self.message = message
+
+        if self.action == Action.SEND_GROUP_MSG and self.group_id is None:
+            raise ValueError("Action 'SEND_GROUP_MESSAGE' requires a 'group_id'.")
+        if self.action == Action.SEND_PRIVATE_MSG and self.user_id is None:
+            raise ValueError("Action 'SEND_PRIVATE_MESSAGE' requires a 'user_id'.")
+
+    def to_dict(self):
+        data = {
+            "name": self.name,
+            "action": self.action.value,
+            "message": self.message,
+        }
+        if self.action == Action.SEND_GROUP_MSG:
+            data["group_id"] = self.group_id
+        elif self.action == Action.SEND_PRIVATE_MSG:
+            data["user_id"] = self.user_id
+
+        return {k: v for k, v in data.items() if v is not None}
+
+    @classmethod
+    def from_dict(cls, data: dict) -> "Signal":
+        """
+        从字典安全地创建一个 Signal 实例
+        """
+        try:
+            data_copy = data.copy()
+
+            name = data_copy.pop("name")
+            action_value = data_copy.pop("action")
+            action = Action(action_value)
+            return cls(name=name, action=action, **data_copy)
+
+        except KeyError as e:
+            raise ValueError(f"字典中缺少必要的键: {e}") from e
+        except ValueError as e:
+            raise ValueError(f"字典中的 'action' 值无效: {e}") from e
+        except TypeError as e:
+            raise TypeError(f"创建 Signal 失败，参数类型错误: {e}") from e
+
+
+async def register_signal(signal: Signal):
+    """注册一个信号"""
+    async with aiofiles.open(SIGNAL_PATH, "w", encoding="utf-8") as f:
+        await f.write(json.dumps(signal.to_dict(), ensure_ascii=False))
+
+
+from nonebot import get_driver
+
+driver = get_driver()
+
+
+@driver.on_bot_connect
+async def trigger_signal(bot: Bot):
+    """触发信号文件"""
+    if not SIGNAL_PATH.is_file():
+        return
+    async with aiofiles.open(SIGNAL_PATH, "r", encoding="utf-8") as f:
+        data = json.loads(await f.read())
+    signal = Signal.from_dict(data)
+    if signal.action == Action.SEND_PRIVATE_MSG:
+        await bot.send_private_msg(user_id=signal.user_id, message=signal.message)
+    elif signal.action == Action.SEND_GROUP_MSG:
+        await bot.send_group_msg(group_id=signal.group_id, message=signal.message)
